3059d9338fef80257e4d4e8b2c5a5839475684cb
Mark Rendle <mark@markrendle.net>
2014-07-18 12:09:14 +0100

    Only disable when using Microsoft.TeamFoundation.Git.Provider

--- src/NoGit/NoGitPackage.cs
+++ src/NoGit/NoGitPackage.cs
@@ -1,32 +1,23 @@
-﻿using System;
-using System.Diagnostics;
-using System.Globalization;
-using System.Runtime.InteropServices;
-using System.ComponentModel.Design;
-using Microsoft.Win32;
-using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio.OLE.Interop;
-using Microsoft.VisualStudio.Shell;
-
-namespace Zudio.NoGit
+﻿namespace Zudio.NoGit
 {
-    using System.ComponentModel.Composition;
-    using System.Text;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.Runtime.InteropServices;
     using EnvDTE;
-    using Microsoft.VisualStudio.Settings;
-    using Microsoft.VisualStudio.Shell.Settings;
-    using IServiceProvider = System.IServiceProvider;
+    using Microsoft.VisualStudio;
+    using Microsoft.VisualStudio.Shell;
+    using Microsoft.VisualStudio.Shell.Interop;
 
     /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    ///
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the 
-    /// IVsPackage interface and uses the registration attributes defined in the framework to 
-    /// register itself and its components with the shell.
+    ///     This is the class that implements the package exposed by this assembly.
+    ///     The minimum requirement for a class to be considered a valid package for Visual Studio
+    ///     is to implement the IVsPackage interface and register itself with the shell.
+    ///     This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    ///     to do it: it derives from the Package class that provides the implementation of the
+    ///     IVsPackage interface and uses the registration attributes defined in the framework to
+    ///     register itself and its components with the shell.
     /// </summary>
     // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
     // a package.
@@ -38,58 +29,40 @@ namespace Zudio.NoGit
     [Guid(GuidList.guidNoGitPkgString)]
     public sealed class NoGitPackage : Package, IVsSolutionEvents
     {
-        private IVsSolution _vsSolution;
         private uint _cookie;
+        private IVsSolution _vsSolution;
+
         /// <summary>
-        /// Default constructor of the package.
-        /// Inside this method you can place any initialization code that does not require 
-        /// any Visual Studio service because at this point the package object is created but 
-        /// not sited yet inside Visual Studio environment. The place to do all the other 
-        /// initialization is the Initialize method.
+        ///     Default constructor of the package.
+        ///     Inside this method you can place any initialization code that does not require
+        ///     any Visual Studio service because at this point the package object is created but
+        ///     not sited yet inside Visual Studio environment. The place to do all the other
+        ///     initialization is the Initialize method.
         /// </summary>
         public NoGitPackage()
         {
-            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this.ToString()));
+            Debug.WriteLine("Entering constructor for: {0}", ToString());
         }
 
-
-
-        /////////////////////////////////////////////////////////////////////////////
-        // Overridden Package Implementation
         #region Package Members
 
         /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        ///     Initialization of the package; this method is called right after the package is sited, so this is the place
+        ///     where you can put all the initialization code that rely on services provided by VisualStudio.
         /// </summary>
         protected override void Initialize()
         {
-            Debug.WriteLine (string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
+            Debug.WriteLine("Entering Initialize() of: {0}", ToString());
             base.Initialize();
 
-            _vsSolution = (IVsSolution)GetService(typeof (SVsSolution));
+            _vsSolution = (IVsSolution) GetService(typeof (SVsSolution));
             _vsSolution.AdviseSolutionEvents(this, out _cookie);
         }
+
         #endregion
 
-        /// <summary>
-        /// This function is the callback used to execute a command when the a menu item is clicked.
-        /// See the Initialize method to see how the menu item is associated to this function using
-        /// the OleMenuCommandService service and the MenuCommand class.
-        /// </summary>
-        private void MenuItemCallback(object sender, EventArgs e)
-        {
-            
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && _cookie != 0)
-            {
-                _vsSolution.UnadviseSolutionEvents(_cookie);
-            }
-            base.Dispose(disposing);
-        }
+        /////////////////////////////////////////////////////////////////////////////
+        // Overridden Package Implementation
 
         public int OnAfterOpenProject(IVsHierarchy pHierarchy, int fAdded)
         {
@@ -151,47 +124,27 @@ namespace Zudio.NoGit
             return VSConstants.S_OK;
         }
 
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && _cookie != 0)
+            {
+                _vsSolution.UnadviseSolutionEvents(_cookie);
+            }
+            base.Dispose(disposing);
+        }
+
         private void SetScciProviderInactive()
         {
-            var getProvider = GetService( typeof( IVsRegisterScciProvider ) ) as IVsGetScciProviderInterface;
-            if ( getProvider != null )
+
+            var getProvider = GetService(typeof (IVsRegisterScciProvider)) as IVsGetScciProviderInterface;
+            if (getProvider != null)
             {
-                var provider = getProvider.GetProvider();
-                if ( provider != null )
+                IVsSccProvider provider = getProvider.GetProvider();
+                if (provider != null && provider.GetType().Namespace == "Microsoft.TeamFoundation.Git.Provider")
                 {
                     provider.SetInactive();
                 }
             }
-        }
-    }
-
-    internal static class VsGetScciProviderExtensions
-    {
-        public static IVsSccProvider GetProvider(this IVsGetScciProviderInterface getScciProvider)
-        {
-            IVsSccProvider provider = null;
-            IntPtr pUnk = IntPtr.Zero;
-            try
-            {
-                pUnk = getScciProvider.GetInterface(typeof(IVsSccProvider).GUID);
-                if (pUnk != IntPtr.Zero)
-                    provider = Marshal.GetObjectForIUnknown(pUnk) as IVsSccProvider;
-            }
-            finally
-            {
-                if (pUnk != IntPtr.Zero)
-                {
-                    Marshal.Release(pUnk);
-                }
-            }
-            return provider;
-        }
-
-        private static IntPtr GetInterface(this IVsGetScciProviderInterface getScciProvider, Guid guid)
-        {
-            IntPtr pvv;
-            getScciProvider.GetSourceControlProviderInterface(ref guid, out pvv);
-            return pvv;
         }
     }
 }
---
+++ src/NoGit/VsGetScciProviderExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace Zudio.NoGit
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell.Interop;
+
+    internal static class VsGetScciProviderExtensions
+    {
+        public static IVsSccProvider GetProvider(this IVsGetScciProviderInterface getScciProvider)
+        {
+            IVsSccProvider provider = null;
+            IntPtr pUnk = IntPtr.Zero;
+            try
+            {
+                pUnk = getScciProvider.GetInterface(typeof (IVsSccProvider).GUID);
+                if (pUnk != IntPtr.Zero)
+                    provider = Marshal.GetObjectForIUnknown(pUnk) as IVsSccProvider;
+            }
+            finally
+            {
+                if (pUnk != IntPtr.Zero)
+                {
+                    Marshal.Release(pUnk);
+                }
+            }
+            return provider;
+        }
+
+        private static IntPtr GetInterface(this IVsGetScciProviderInterface getScciProvider, Guid guid)
+        {
+            IntPtr pvv;
+            getScciProvider.GetSourceControlProviderInterface(ref guid, out pvv);
+            return pvv;
+        }
+    }
+}
